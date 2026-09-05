d65b8248670bc3011bdc56c3dc1652a7e4f5dd01
Luke Benhase <100175862+LukeBenhase@users.noreply.github.com>
2026-09-05 10:53:27 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int min = INT_MAX;
+
+        for (int price : prices){
+            if (price < min){
+                min = price;
+            }
+            // calculate if this profit is bigger than max profit
+            int profit = price - min;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+
+        return maxProfit;
+    }
+};
