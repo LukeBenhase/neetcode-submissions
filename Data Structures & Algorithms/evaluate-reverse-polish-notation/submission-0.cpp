32e576df305222043abd67a836e9539549f00503
Luke Benhase <100175862+LukeBenhase@users.noreply.github.com>
2026-09-22 05:41:06 -0400

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int result = 0;
+        vector<int> values;
+        int topValuesPtr = 0;
+        for ( string t : tokens){
+            if (t == "+" || t == "-" || t == "/" || t == "*"){
+                // do that operation on the top 2 values
+                if (topValuesPtr < 2){
+                    cout << "the expression is not valid" << endl;
+                    return 0;
+                }
+                int temp2 = values.back();
+                values.pop_back();
+                int temp1 = values.back();
+                values.pop_back();
+                if(t == "+" ){
+                    values.push_back(temp1 + temp2);
+                }
+                else if( t == "-" ){
+                    values.push_back(temp1 - temp2);
+                }
+                else if( t == "/" ){
+                    values.push_back(temp1 / temp2);
+                }
+                else if( t == "*" ){
+                    values.push_back(temp1 * temp2);
+                }
+                topValuesPtr--;
+            }
+            else {
+                // t is a number
+                values.push_back(stoi(t));
+                cout << values.back() << endl;
+                topValuesPtr++;
+            }
+        }
+        return values.back();
+;
+    }
+};
